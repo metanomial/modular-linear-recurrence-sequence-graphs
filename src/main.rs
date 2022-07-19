2c8a495677f01cb2e51efbdfc074984c125b0fcb
Benjamin Herman <benjamin@metanomial.com>
2022-07-19 07:54:44 -0700

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use std::{
-    collections::{HashMap, HashSet},
-    env,
+    // collections::{HashMap, HashSet},
+    num::NonZeroUsize,
     path::PathBuf,
+    process,
 };
 
 use clap::Parser;
@@ -9,120 +10,122 @@ use clap::Parser;
 #[cfg(test)]
 mod tests;
 
-/// Simple program to greet a person
+/// Calculate and render directed graphs from modular linear recurrence
+/// sequences with constant coefficients.
 #[derive(Parser, Debug)]
 #[clap(author, version, about)]
 struct Cli {
-    /// Print the cycles
-    #[clap(subcommand)]
-    program: Program,
-}
+    /// Sequence modulus. Must be a non-zero positive integer.
+    #[clap(value_parser)]
+    modulus: NonZeroUsize,
 
-#[derive(clap::Subcommand, Debug)]
-enum Program {
-    /// Compute the cycles of all Lucas sequences modulo N
-    Cycles {
-        #[clap(short, long, value_parser)]
-        modulus: u32,
-    },
+    /// Constant coefficients of the recurrence relation. Must be a list of
+    /// colon-separated non-negative integers. The coefficient's index
+    /// corresponds to the n-minus-i-th term in the recurrence relation.
+    /// The default value is "1:1"
+    #[clap(value_parser)]
+    coefficients: Option<String>,
 
-    /// Render the pairs of all Lucas sequences modulo N, colored by associated cycle.
-    Render {
-        #[clap(short, long, value_parser)]
-        modulus: u32,
-
-        /// Image output path
-        #[clap(short, long)]
-        output: Option<PathBuf>,
-    },
+    /// Render a mosaic image from sequence pairs and output at the given path.
+    /// Only available for recurrence relations of first or second order.
+    #[clap(short, long)]
+    render: Option<PathBuf>,
 }
 
 fn main() {
     let cli = Cli::parse();
-    match cli.program {
-        Program::Cycles { modulus } => print_cycles(modulus),
-        Program::Render { modulus, output } => {
-            let output = output.unwrap_or(env::current_dir().unwrap());
-            render_pairs(modulus, output);
-        }
+    let coefficients = parse_coefficients(cli.coefficients);
+
+    if cli.render.is_some() && coefficients.len() != 2 {
+        eprintln!("Mosaic rendering is only supported for second order recurse relations");
+        process::exit(1);
     }
+
+    // let mut cycle_number = 0;
+    // for i in 0..modulus {
+    //     for j in 0..modulus {
+    //         if found.contains(&(i, j)) {
+    //             continue;
+    //         }
+    //         let mut cycle = Vec::<u32>::new();
+    //         let mut a = i;
+    //         let mut b = j;
+    //         while !found.contains(&(a, b)) {
+    //             found.insert((a, b));
+    //             cycle.push(a);
+    //             pairs.insert((a, b), cycle_number);
+    //             let new = (a + b) % modulus;
+    //             a = b;
+    //             b = new;
+    //         }
+    //         cycle_number += 1;
+    //         cycles.push(cycle);
+    //     }
+    // }
 }
 
-fn print_cycles(modulus: u32) {
-    println!("Base {}", modulus);
-
-    let cycles = CycleSet::compute(modulus).cycles;
-    let mut cycle_lengths: Vec<usize> = cycles.iter().map(|c| c.len()).collect();
-    cycle_lengths.sort();
-
-    println!("Cycle count: {}", cycles.len());
-    println!(
-        "Cycle lengths: {}",
-        cycle_lengths
-            .iter()
-            .map(|l| l.to_string())
-            .collect::<Vec<String>>()
-            .join(", ")
-    );
-
-    for cycle in cycles {
-        println!(
-            "[{}]: {}",
-            cycle.len(),
-            cycle
-                .iter()
-                .map(|n| n.to_string())
-                .collect::<Vec<String>>()
-                .join(" ")
-        );
-    }
-}
-
-fn render_pairs(modulus: u32, output: PathBuf) {
-    println!("Base {}", modulus);
-
-    let cycle_set = CycleSet::compute(modulus);
-    let cycles = cycle_set.cycles;
-    let pairs = cycle_set.pairs;
-
-    println!("Cycle count: {}", cycles.len());
-
-    todo!();
-}
-
-struct CycleSet {
-    cycles: Vec<Vec<u32>>,
-    pairs: HashMap<(u32, u32), usize>,
-}
-
-impl CycleSet {
-    fn compute(modulus: u32) -> CycleSet {
-        let mut found = HashSet::<(u32, u32)>::new();
-        let mut pairs = HashMap::<(u32, u32), usize>::new();
-        let mut cycles = Vec::<Vec<u32>>::new();
-        let mut cycle_number = 0;
-
-        for i in 0..modulus {
-            for j in 0..modulus {
-                if found.contains(&(i, j)) {
-                    continue;
+fn parse_coefficients(string: Option<String>) -> Vec<usize> {
+    match string {
+        Some(string) => {
+            let mut coefficients = Vec::<usize>::new();
+            for sym in string.split(":") {
+                match sym.trim().parse() {
+                    Ok(i) => coefficients.push(i),
+                    Err(e) => {
+                        eprintln!("Invalid coefficient \"{}\": {}", sym, e);
+                        process::exit(1)
+                    }
                 }
-                let mut cycle = Vec::<u32>::new();
-                let mut a = i;
-                let mut b = j;
-                while !found.contains(&(a, b)) {
-                    found.insert((a, b));
-                    cycle.push(a);
-                    pairs.insert((a, b), cycle_number);
-                    let new = (a + b) % modulus;
-                    a = b;
-                    b = new;
-                }
-                cycle_number += 1;
-                cycles.push(cycle);
             }
+            coefficients
         }
-
-        CycleSet { cycles, pairs }
+        None => vec![1, 1],
     }
 }
+
+// fn map_pairs(modulus: usize, coefficients: Vec<usize>) -> HashMap<Vec<usize>, usize> {
+//     let mut pairs = HashMap::<Vec<usize>, usize>::new();
+//     pairs
+// }
+
+// fn print_cycles(modulus: u32) {
+//     println!("Base {}", modulus);
+
+//     let cycles = CycleSet::compute(modulus).cycles;
+//     let mut cycle_lengths: Vec<usize> = cycles.iter().map(|c| c.len()).collect();
+//     cycle_lengths.sort();
+
+//     println!("Cycle count: {}", cycles.len());
+//     println!(
+//         "Cycle lengths: {}",
+//         cycle_lengths
+//             .iter()
+//             .map(|l| l.to_string())
+//             .collect::<Vec<String>>()
+//             .join(", ")
+//     );
+
+//     for cycle in cycles {
+//         println!(
+//             "[{}]: {}",
+//             cycle.len(),
+//             cycle
+//                 .iter()
+//                 .map(|n| n.to_string())
+//                 .collect::<Vec<String>>()
+//                 .join(" ")
+//         );
+//     }
+// }
+
+// fn render_pairs(modulus: u32, output: PathBuf) {
+//     println!("Base {}", modulus);
+
+//     let cycle_set = CycleSet::compute(modulus);
+//     let cycles = cycle_set.cycles;
+//     let pairs = cycle_set.pairs;
+
+//     println!("Cycle count: {}", cycles.len());
+
+//     todo!();
+// }
--- src/tests.rs
+++ src/tests.rs
@@ -1,28 +1,28 @@
-use super::*;
+// use super::*;
 
-#[test]
-fn test_compute_cycles() {
-    let cycle_set = CycleSet::compute(1);
-    assert_eq!(cycle_set.cycles, vec![vec![0]]);
+// #[test]
+// fn test_compute_cycles() {
+//     let cycle_set = CycleSet::compute(1);
+//     assert_eq!(cycle_set.cycles, vec![vec![0]]);
 
-    let cycle_set = CycleSet::compute(10);
-    assert_eq!(
-        cycle_set.cycles,
-        vec![
-            vec![0],
-            vec![
-                0, 1, 1, 2, 3, 5, 8, 3, 1, 4, 5, 9, 4, 3, 7, 0, 7, 7, 4, 1, 5, 6, 1, 7, 8, 5, 3, 8,
-                1, 9, 0, 9, 9, 8, 7, 5, 2, 7, 9, 6, 5, 1, 6, 7, 3, 0, 3, 3, 6, 9, 5, 4, 9, 3, 2, 5,
-                7, 2, 9, 1
-            ],
-            vec![0, 2, 2, 4, 6, 0, 6, 6, 2, 8, 0, 8, 8, 6, 4, 0, 4, 4, 8, 2],
-            vec![0, 5, 5],
-            vec![1, 3, 4, 7, 1, 8, 9, 7, 6, 3, 9, 2],
-            vec![2, 6, 8, 4]
-        ]
-    );
-    assert_eq!(cycle_set.pairs.len(), 100);
+//     let cycle_set = CycleSet::compute(10);
+//     assert_eq!(
+//         cycle_set.cycles,
+//         vec![
+//             vec![0],
+//             vec![
+//                 0, 1, 1, 2, 3, 5, 8, 3, 1, 4, 5, 9, 4, 3, 7, 0, 7, 7, 4, 1, 5, 6, 1, 7, 8, 5, 3, 8,
+//                 1, 9, 0, 9, 9, 8, 7, 5, 2, 7, 9, 6, 5, 1, 6, 7, 3, 0, 3, 3, 6, 9, 5, 4, 9, 3, 2, 5,
+//                 7, 2, 9, 1
+//             ],
+//             vec![0, 2, 2, 4, 6, 0, 6, 6, 2, 8, 0, 8, 8, 6, 4, 0, 4, 4, 8, 2],
+//             vec![0, 5, 5],
+//             vec![1, 3, 4, 7, 1, 8, 9, 7, 6, 3, 9, 2],
+//             vec![2, 6, 8, 4]
+//         ]
+//     );
+//     assert_eq!(cycle_set.pairs.len(), 100);
 
-    let cycle_set = CycleSet::compute(123);
-    assert_eq!(cycle_set.pairs.len(), 15_129);
-}
+//     let cycle_set = CycleSet::compute(123);
+//     assert_eq!(cycle_set.pairs.len(), 15_129);
+// }
